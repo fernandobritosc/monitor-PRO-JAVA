2c5b0af0c6587fd107165cc3311327ce84aafdf9
fernandobritosc <fernandobritosc@gmail.com>
2026-02-05 16:30:42 -0300

    'Auto-save'

---
+++ views/index.ts
@@ -0,0 +1,14 @@
+
+export { default as Login } from './Login';
+export { default as HomeView } from './HomeView';
+export { StudyForm } from './StudyForm';
+export { default as History } from './History';
+export { default as Revisoes } from './Revisoes';
+export { default as Simulados } from './Simulados';
+export { default as Configurar } from './Configurar';
+export { default as WeeklyGuide } from './WeeklyGuide';
+export { default as QuestionsBank } from './QuestionsBank';
+export { default as Reports } from './Reports';
+export { default as Onboarding } from './Onboarding';
+export { default as EditalProgress } from './EditalProgress';
+export { default as Flashcards } from './Flashcards';
